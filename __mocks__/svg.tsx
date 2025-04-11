17f65f30a1e52496b1f5144edf3109d07c50c7ef
Rémy Sabliet <sabliet@gmail.com>
2025-04-11 16:47:34 +0200

    🔥 Remove unused __mock__ folder

--- __mocks__/svg.tsx
+++
@@ -1,6 +0,0 @@
-import React, { HTMLAttributes } from 'react';
-
-const SvgStab = (props: HTMLAttributes<HTMLImageElement>) => <img {...props} />;
-export const ReactComponent = 'img';
-
-export default SvgStab;
